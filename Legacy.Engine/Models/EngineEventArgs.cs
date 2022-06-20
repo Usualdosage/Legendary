f51c220cde55fa50b03632282849254d083e0b93
Usualdosage <matt@usualdosage.com>
2022-06-20 17:21:43 -0400

    Engine and server refactored to be single instances.

---
+++ Legacy.Engine/Models/EngineEventArgs.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="EngineEventArgs.cs" company="Legendary">
+//  Copyright © 2022 Legendary
+//  All rights are reserved. Reproduction or transmission in whole or
+//  in part, in any form or by any means, electronic, mechanical or
+//  otherwise, is prohibited without the prior written consent of
+//  the copyright owner.
+// </copyright>
+
+namespace Legendary.Engine.Models
+{
+    using System;
+
+    public class EngineEventArgs : EventArgs
+	{
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommunicationEventArgs"/> class.
+        /// </summary>
+        /// <param name="socketId">The socket Id.</param>
+        /// <param name="message">The message.</param>
+        public EngineEventArgs(int gameTicks, int gameHour)
+        {
+            this.GameTicks = gameTicks;
+            this.GameHour = gameHour;
+        }
+
+        /// <summary>
+        /// Gets the message.
+        /// </summary>
+        public int GameHour { get; private set; }
+
+        /// <summary>
+        /// Gets the message.
+        /// </summary>
+        public int GameTicks { get; private set; }
+    }
+}
+
---
+++ Legendary.Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Legendary.Core.Models;
+using Legendary.Core.Types;
+using Newtonsoft.Json;
+
+namespace Legendary.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mob = new Mobile()
+            {
+                FirstName = "Virgil",
+                Level = 60,
+                Description = "You see a man in brown robes standing before you. He appears eager to guide you through Hell.",
+                Health = new MaxCurrent(10000, 10000),
+                Mana = new MaxCurrent(10000, 10000),
+                Movement = new MaxCurrent(10000, 10000),
+                MobileId = 1,
+                Currency = 1000,
+                Experience = 10000000,
+                MobileFlags = new List<MobileFlags>()               
+            };
+
+            var jsonObj = JsonConvert.SerializeObject(mob);
+        }
+    }
+}
