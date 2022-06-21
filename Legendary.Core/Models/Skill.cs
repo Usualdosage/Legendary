838f20869ed4e2a6f2bf161e3755f4d311b2b29b
Usualdosage <matt@usualdosage.com>
2022-06-21 19:03:51 -0400

    Added skill and spell classes.

---
+++ Legendary.Core/Models/Skill.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="Skill.cs" company="Legendary">
+//  Copyright © 2022 Legendary
+//  All rights are reserved. Reproduction or transmission in whole or
+//  in part, in any form or by any means, electronic, mechanical or
+//  otherwise, is prohibited without the prior written consent of
+//  the copyright owner.
+// </copyright>
+
+namespace Legendary.Core.Models
+{
+    using System;
+
+    public class Skill
+	{
+		public Skill()
+		{
+		}
+
+        public string? Name { get; set; }
+    }
+}
+
